87da0d5355bc263633f34e8471d8b5d3ed6cb953
Antonio Sejas <antonio@sejas.es>
2016-08-03 17:21:56 +0100

    adding class map street view ios and detail controller

---
+++ Playas/Playas/DetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailViewController.swift
+//  Playas
+//
+//  Created by Antonio Sejas on 3/8/16.
+//  Copyright © 2016 Antonio Sejas. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var img: UIImageView!
+
+    var beach:Beach?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let beach = beach {
+         StreetMap.getStreetMap(Int(img.frame.width), sizey: Int(img.frame.height), coordinate: beach.coordinate, completionHandlerForGETData: { (image, data, error) in
+            guard error != nil else {
+                return
+            }
+            performUIUpdatesOnMain({ 
+                self.img.image = image
+            })
+         })
+        }
+    }
+}
---
+++ Playas/Playas/StreetMap.swift
@@ -0,0 +1,20 @@
+//
+//  StreetMap.swift
+//  Playas
+//
+//  Created by Antonio Sejas on 3/8/16.
+//  Copyright © 2016 Antonio Sejas. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+//URL EJEMPLO
+//https://maps.googleapis.com/maps/api/streetview?size=400x400&location=40.720032,-73.988354&fov=90&heading=235&pitch=10&key=
+class StreetMap: NSObject {
+    
+    static func getStreetMap(sizex:Int, sizey:Int, coordinate: CLLocationCoordinate2D, completionHandlerForGETData:(image: UIImage, data: NSData, error: NSError?) -> Void ){
+        let urlString = "https://maps.googleapis.com/maps/api/streetview?size=400x400&location=40.720032,-73.988354&fov=90&heading=235&pitch=10&key=AIzaSyA4dTeaH6_PP_mYz-o0L_wvzVJF5H0dy3U"
+        NetworkHelper.sharedInstance.getImage(urlString, completionHandlerForGETData: completionHandlerForGETData)
+    }
+}
