5d55eabed27ffd98b77c88b015f15db0bd0b62e8
Antonio Sejas <antonio@sejas.es>
2016-08-10 22:04:52 +0100

    added <key>NSAppTransportSecurity</key> <dict> <key>NSAllowsArbitraryLoads</key> <true/> </dict> to info.plist to allow download json from firebase

--- Playas/Playas/BeachesNetwork.swift
+++ Playas/Playas/BeachesNetwork.swift
@@ -17,8 +17,31 @@ class BeachesNetwork: NSObject {
     func getCachedLocations() -> [Beach] {
         return beaches
     }
-    
     func downloadLocationsWithCompletion(completionHandler:(beaches: [Beach], error: NSError? ) -> Void) {
+        let urlString = "https://beaches-26a4e.firebaseio.com/grancanariabeaches/resources.json"
+        NetworkHelper.sharedInstance.getRequest(urlString, headers: nil) { (result, error) in
+            guard nil == error else {
+                print ("error")
+                completionHandler(beaches: [Beach](), error: error)
+                return
+            }
+             var beaches = [Beach]()
+            guard let beachesDictionary = result as? [[String:AnyObject]] else {
+                completionHandler(beaches: [Beach](), error: NSError(domain: "downloadLocationsWithCompletion", code: 0, userInfo: nil))
+                return
+            }
+            print("beachesDictionary",beachesDictionary)
+            for beachDictionary in beachesDictionary {
+                beaches.append(Beach(dictionary: beachDictionary))
+            }
+            
+            self.beaches = beaches
+            completionHandler(beaches: beaches, error: nil)
+
+            
+        }
+    }
+    func readLocalLocationsWithCompletion(completionHandler:(beaches: [Beach], error: NSError? ) -> Void) {
 //        let url = NSBundle.mainBundle().URLForResource("playas-gran-canaria", withExtension: "json")!
         let urlString = NSBundle.mainBundle().pathForResource("playas-gran-canaria", ofType: "json")
         print("url string: ",urlString)
