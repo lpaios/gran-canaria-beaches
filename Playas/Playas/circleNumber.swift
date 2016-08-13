6a08c87a8350937dfc80e73e8ef6a18f09673a04
Antonio Sejas <antonio@sejas.es>
2016-08-14 00:52:54 +0100

    vertical stack view for detal view

---
+++ Playas/Playas/circleNumber.swift
@@ -0,0 +1,20 @@
+//
+//  circleNumber.swift
+//  Playas
+//
+//  Created by Antonio Sejas on 13/8/16.
+//  Copyright © 2016 Antonio Sejas. All rights reserved.
+//
+
+import UIKit
+
+class circleNumber: UIView {
+
+//    override func drawRect(rect: CGRect) {
+//        // Drawing code
+//        return CGRect(x: <#T##CGFloat#>, y: <#T##CGFloat#>, width: <#T##CGFloat#>, height: <#T##CGFloat#>)
+//    }
+    
+    
+
+}
